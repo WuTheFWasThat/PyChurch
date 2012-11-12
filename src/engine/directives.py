b8f6b1da3d9edc47307431e68eb3038a70f9b246
Jeff Wu <wuthefwasthat@gmail.com>
2012-11-12 05:51:31 -0500

    rpythonified

--- src/engine/directives.py
+++ src/engine/directives.py
@@ -115,27 +115,27 @@ def reset():
 
   assume('mem', xrp(mem_XRP()), True)
 
-  #"""DEFINITION OF DP"""
-  #assume('DP_uncollapsed', \
-  #       function(['concentration', 'basemeasure'], \
-  #                let([('sticks', apply(var('mem'), function(['j'], apply(var('beta'), [num_expr(1), var('concentration')])))),
-  #                     ('atoms',  apply(var('mem'), function(['j'], apply(var('basemeasure'), [])))),
-  #                     ('loop', \
-  #                      function(['j'], \
-  #                               ifelse(bernoulli(apply(var('sticks'), [var('j')])), \
-  #                                      apply(var('atoms'), [var('j')]), \
-  #                                      apply(var('loop'), [var('j')+num_expr(1)])))) \
-  #                    ], \
-  #                    function([], apply(var('loop'), [num_expr(1)])))),
-  #       True) 
+  """DEFINITION OF DP"""
+  assume('DP_uncollapsed', \
+         function(['concentration', 'basemeasure'], \
+                  let([('sticks', apply(var('mem'), [function(['j'], apply(var('beta'), [num_expr(1), var('concentration')]))])),
+                       ('atoms',  apply(var('mem'), [function(['j'], apply(var('basemeasure'), []))])),
+                       ('loop', \
+                        function(['j'], 
+                                 ifelse(apply(var('bernoulli'), [apply(var('sticks'), [var('j')])]), \
+                                        apply(var('atoms'), [var('j')]), \
+                                        apply(var('loop'), [op('+', [var('j'), num_expr(1)])])))) \
+                      ], \
+                      function([], apply(var('loop'), [num_expr(1)])))),
+         True) 
 
-  #"""DEFINITION OF ONE ARGUMENT DPMEM"""
-  #assume('DPmem_uncollapsed', \
-  #       function(['concentration', 'proc'], \
-  #                let([('restaurants', \
-  #                      apply(var('mem'), function(['args'], apply(var('DP'), [var('concentration'), function([], apply(var('proc'), [var('args')]))]))))], \
-  #                    function(['args'], apply(var('restaurants'), [var('args')])))), 
-  #       True) 
+  """DEFINITION OF ONE ARGUMENT DPMEM"""
+  assume('DPmem_uncollapsed', \
+         function(['concentration', 'proc'], \
+                  let([('restaurants', \
+                        apply(var('mem'), [function(['args'], apply(var('DP'), [var('concentration'), function([], apply(var('proc'), [var('args')]))]))]))], \
+                      function(['args'], apply(var('restaurants'), [var('args')])))), 
+         True) 
 
   memory.reset()
 
