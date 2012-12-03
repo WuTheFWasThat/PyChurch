26043e9a8e03cc96d5964312f07057ba0024eaca
Jeff Wu <wuthefwasthat@gmail.com>
2012-12-03 01:25:04 -0500

    made verbose/nonverbose option for server

--- src/server.py
+++ src/server.py
@@ -13,10 +13,12 @@ parser.add_argument('-e', default='traces', dest = 'engine',
                    help='Type of engine (db, traces, reduced_trace)')
 parser.add_argument('-s', action='store_true', dest = 'use_socket',
                    help = 'Use the socket RIPL.  Remember to change the PID!')
+parser.add_argument('-v', action='store_true', dest = 'verbose',
+                   help = 'Print server activity')
 
-args = parser.parse_args()
+flags = parser.parse_args()
 
-engine_type = args.engine
+engine_type = flags.engine
 if engine_type in ['rt', 'reduced', 'reduced_trace', 'reduced_traces', 'reducedtrace', 'reducedtraces']:
   engine_type = 'reduced traces'
 elif engine_type in ['t', 'trace', 'traces']:
@@ -26,13 +28,24 @@ elif engine_type in ['r', 'db', 'randomdb']:
 else:
   raise Exception("Engine %s is not implemented" % engine_type)
 
-if args.use_socket:
+if flags.use_socket:
   ripl = myripl.SocketRIPL(engine_type)
 else:
   ripl = myripl.DirectRIPL(engine_type)
 
 global app
 app = flask.Flask(__name__)
+
+def print_verbose(header, message = None, args = None):
+  if flags.verbose:
+    print
+    print header
+    if args is not None:
+      for key in args:
+        print key, ": ", args[key]
+    if message is not None:
+      print message 
+    print "\n----------\n"
 
 def get_response(string):
     resp = make_response(string)
@@ -44,98 +57,97 @@ def get_response(string):
 @app.route('/', methods=['GET'])
 def index():
     directives = ripl.report_directives()
+    print_verbose("DIRECTIVES REPORT", directives)
     return get_response(json.dumps(directives))
 
 @app.route('/', methods=['POST'])
 # DELETE
 def clear():
     ripl.clear()
-    print "CLEARED"
+    print_verbose("CLEARED")
     return get_response(json.dumps({}))
 
 @app.route('/<int:did>', methods=['POST'])
 # DELETE
 def forget(did):
     ripl.forget(did)
-    print "FORGOTTEN"
+    print_verbose("FORGOTTEN")
     return get_response(json.dumps({}))
 
 @app.route('/<int:did>', methods=['GET'])
 def report_value(did):
-    print "REPORTED VALUE"
-    print "did: ", did
     d = ripl.report_value(did)
-    print d
+    print_verbose("REPORT VALUE", d, {'did': did})
     return get_response(json.dumps(d))
 
 @app.route('/memory', methods=['GET'])
 def memory():
     s = ripl.memory()
+    print_verbose("MEMORY", s)
     return get_response(json.dumps({"memory": s}))
 
 @app.route('/entropy', methods=['GET'])
 def entropy():
     h = ripl.entropy()
+    print_verbose("ENTROPY", h)
     return get_response(json.dumps({"random_choices": h}))
 
 @app.route('/mhstats/aggregated', methods=['GET'])
 def mhstats_aggregated():
     d = ripl.mhstats_aggregated()
+    print_verbose("MHSTATS AGGREGATED", d)
     return get_response(json.dumps(d))
 
 @app.route('/mhstats/detailed', methods=['GET'])
 def mhstats_detailed():
     d = ripl.mhstats_detailed()
+    print_verbose("MHSTATS DETAILED", d)
     return get_response(json.dumps(d))
 
 @app.route('/mhstats/detailed/on', methods=['POST'])
 def mhstats_on(did):
     ripl.mhstats_on(did)
+    print_verbose("MHSTATS DETAILED ON")
     return get_response(json.dumps({"MESSAGE": "Mhstats On"}))
 
 @app.route('/mhstats/detailed/off', methods=['POST'])
 def mhstats_off(did):
     ripl.mhstats_off(did)
+    print_verbose("MHSTATS DETAILED OFF")
     return get_response(json.dumps({"MESSAGE": "Mhstats Off"}))
 
 @app.route('/logscore', methods=['GET'])
 def logscore():
     p = ripl.logscore()
+    print_verbose("LOGSCORE", p)
     return get_response(json.dumps({"directive_logscore": p}))
 
 @app.route('/logscore/<int:did>', methods=['GET'])
 def logscore_observe(did):
     p = ripl.logscore(did)
+    print_verbose("LOGSCORE", p, {"did": did})
     return get_response(json.dumps({"directive_logscore": p}))
 
 @app.route('/seed', methods=['POST'])
 def seed():
     seed = json.loads(request.form["seed"])
-    print "SEED REQUEST"
-    print "seed: ", seed
+    print_verbose("SEED", seed)
     ripl.seed(seed)
     return get_response("Seeded with %s" % str(seed))
 
 @app.route('/infer', methods=['POST'])
 def infer():
-    # FIXME: Define and support a proper config
     MHiterations = json.loads(request.form["MHiterations"])
-    print "INFER REQUEST"
-    print "iterations: " + str(MHiterations)
     t = ripl.infer(MHiterations)
-    print "time: ", t
+    print_verbose("INFER", None, {"iters": MHiterations, "time": t})
     return get_response(json.dumps({"time": t}))
 
 @app.route('/assume', methods=['POST'])
 def assume():
     name_str = json.loads(request.form["name_str"])
     expr_lst = json.loads(request.form["expr_lst"])
-    print "ASSUMPTION REQUEST"
-    print "name_str: " + str(name_str)
-    print "expr_lst: " + str(expr_lst)
     (d_id, val) = ripl.assume(name_str, expr_lst)
-    print "d_id: " + str(d_id)
-    print "val: " + str(val)
+    print_verbose("ASSUME", {"d_id": d_id, "val": val}, {"name": name_str, "expression": expr_lst})
     return get_response(json.dumps({"d_id": d_id,
                        "val": val}))
 
@@ -143,20 +155,15 @@ def assume():
 def observe():
     expr_lst = json.loads(request.form["expr_lst"])
     literal_val = json.loads(request.form["literal_val"])
-    print "OBSERVE"
-    print str(expr_lst)
     d_id = ripl.observe(expr_lst, literal_val)
-    print "d_id: " + str(d_id)
+    print_verbose("OBSERVE", {"d_id": d_id}, {"expression": expr_lst, "obs_val": literal_val})
     return json.dumps({"d_id": d_id})
 
 @app.route('/predict', methods=['POST'])
 def predict():
     expr_lst = json.loads(request.form["expr_lst"])
-    print "PREDICT"
-    print str(expr_lst)
     (d_id, val) = ripl.predict(expr_lst)
-    print "d_id: " + str(d_id)
-    print "val: " + str(val)
+    print_verbose("PREDICT", {"d_id": d_id, "val": val}, {"expression": expr_lst})
     return get_response(json.dumps({"d_id": d_id, "val": val}))
 
 if __name__ == '__main__':
