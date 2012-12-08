05be6b622b3a38f9ec2d8d846005e8eae89b9482
Jeff Wu <wuthefwasthat@gmail.com>
2012-12-07 20:02:44 -0500

    adding ec2 set-up shell script

---
+++ src/setup.sh
@@ -0,0 +1,8 @@
+# SHELL SCRIPT FOR SETUP ON EC2
+
+sudo yum -y install git
+sudo yum -y install easy_install
+
+git clone https://github.com/WuTheFWasThat/Church-interpreter.git
+
+git clone https://github.com/probcomp/ProbabilisticEngineTestSuite.git
